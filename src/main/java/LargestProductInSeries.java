c502d287df5172c62cf940146bee0bf516a00bc4
Alexey <reshetyk@gmail.com>
2016-07-25 00:57:15 +0200

    opt

--- src/main/java/LargestProductInSeries.java
+++ src/main/java/LargestProductInSeries.java
@@ -1,52 +1,73 @@
-import java.util.function.IntBinaryOperator;
-
-import static java.util.Arrays.copyOfRange;
-import static java.util.Arrays.stream;
-
 /**
  * Created by Reshetuyk on 17.07.2016.
  */
 public class LargestProductInSeries {
-//    private int max;
-//
-//    public int findMaxMultiplyAdjacent(int[] series, int limit) {
-//        if (series.length < limit) throw new RuntimeException("limit cannot be larger than series");
-//
-//        int halfLength = series.length / 2;
-//        int multiplyResult;
-//
-//        if (series.length == limit) {
-//            multiplyResult = stream(series).reduce(1, multiple());
-//            return max > multiplyResult ? max : multiplyResult;
-//        }
-//        if (halfLength < limit) {
-//            multiplyResult = findMaxMultiplyAdjacentSimple(series, limit);
-//            return max > multiplyResult ? max : multiplyResult;
-//        }
-//
-//        int[] middle = copyOfRange(series, halfLength - (limit - 1), halfLength + (limit - 1));
-//        int maxInMiddle = findMaxMultiplyAdjacentSimple(middle, limit);
-//        max = maxInMiddle > max ? maxInMiddle : max;
-//        int[] firstHalf = copyOfRange(series, 0, halfLength);
-//        int[] secondHalf = copyOfRange(series, halfLength, series.length);
-//
-//        return stream(firstHalf).sum() > stream(secondHalf).sum()
-//                ? findMaxMultiplyAdjacent(firstHalf, limit)
-//                : findMaxMultiplyAdjacent(secondHalf, limit);
-//    }
+    private int iterations;
+    private int[] series;
+    private int limit;
+    private int max = 0;
 
-    public static int findMaxMultiplyAdjacent(int[] series, int limit) {
-        if (series.length < limit) throw new RuntimeException("limit cannot be larger than series");
+    public LargestProductInSeries(int[] series, int limit) {
+        this.series = series;
+        this.limit = limit;
+    }
 
-        int max = 0;
-        for (int i = 0; i < series.length; i++) {
-            if (i + limit > series.length) break;
-            max = Math.max(stream(copyOfRange(series, i, limit + i)).reduce(1, multiple()), max);
+    public int findMaxMultiplyAdjacentNotOptimized() {
+        validateLimit(series, limit);
+
+        for (int i = 0; i + limit - 1 <= series.length - 1; i++) {
+            int res = multiplySeries(series, i, limit + i - 1);
+            max = Math.max(res, max);
+        }
+        System.out.println("series length=" + series.length + " limit=" + limit + " iterations=" + iterations);
+        return max;
+    }
+
+
+    public int findMaxMultiplyAdjacent() {
+        int res = findMaxMultiplyAdjacent(0, series.length - 1, 0);
+        System.out.println("series length=" + series.length + " limit=" + limit + " iterations=" + iterations);
+        return res;
+    }
+
+    public int findMaxMultiplyAdjacent(int from, int to, int res) {
+        validateLimit(series, limit);
+
+        for (int i = from; i + limit - 1 <= to; i++) {
+            if (res == 0) {
+                res = multiplySeries(series, i, limit + i - 1);
+            }
+            if (i + limit < series.length - 1 && series[i + limit] == 0 || res == 0) {
+                return findMaxMultiplyAdjacent(i + limit + 1, to, 0);
+            }
+
+            if (i + limit > series.length - 1)
+                return Math.max(res, max);
+
+            res = res / series[i] * series[i + limit];
+            iterations = iterations + 2;
+
+            max = Math.max(res, max);
         }
         return max;
     }
 
-    private static IntBinaryOperator multiple() {
-        return (a, b) -> a * b;
+    private static void validateLimit(int[] series, int limit) {
+        if (series.length < limit) throw new RuntimeException("limit cannot be larger than series");
+    }
+
+    private int multiplySeries(int[] series, int from, int to) {
+        validateFromTo(series, from, to);
+        int res = 1;
+        for (int i = from; i <= to; i++) {
+            res *= series[i];
+            ++iterations;
+        }
+        return res;
+    }
+
+    private static void validateFromTo(int[] series, int from, int to) {
+        if (from > series.length || to > series.length)
+            throw new RuntimeException("from or to cannot be bigger then array length");
     }
 }
