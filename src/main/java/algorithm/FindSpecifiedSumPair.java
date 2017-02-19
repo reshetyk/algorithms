30ae6ca3aa4940d2462ab9ada00dd9bfb50ce497
Alexey <reshetyk@gmail.com>
2017-02-19 01:01:45 +0100

    fix and added alternative solution

--- src/main/java/algorithm/FindSpecifiedSumPair.java
+++ src/main/java/algorithm/FindSpecifiedSumPair.java
@@ -11,11 +11,12 @@ import java.util.List;
  */
 public class FindSpecifiedSumPair {
 
-    public static List<Pair<Integer, Integer>> findPairs (Integer[] arr, int sum) {
+    public static List<Pair<Integer, Integer>> findPairs(Integer[] arr, int sum) {
         Arrays.sort(arr);
         List<Integer> arrList = new ArrayList<>(Arrays.asList(arr));
         List<Pair<Integer, Integer>> pairs = new ArrayList<>();
-        for (int i = 0; i < arrList.size(); i++) {
+        int i = 0;
+        while (i < arrList.size()) {
             int current = arrList.get(i);
             int pairToFind = current < 0 ? Math.abs(current) + sum : sum - current;
             int pairIdx = Arrays.binarySearch(arrList.toArray(), pairToFind);
@@ -25,6 +26,26 @@ public class FindSpecifiedSumPair {
                 arrList.remove(pairIdx);
             }
             arrList.remove(i);
+
+        }
+        return pairs;
+    }
+
+    public static List<Pair> findPairs2(Integer[] array, int sum) {
+        Arrays.sort(array);
+        int first = 0;
+        int last = array.length - 1;
+        List<Pair> pairs = new ArrayList<>(array.length);
+        while (first < last) {
+            int s = array[first] + array[last];
+            if (s == sum) {
+                pairs.add(new Pair(array[first], array[last]));
+                first++;
+                last--;
+            } else {
+                if (s < sum) ++first;
+                else --last;
+            }
         }
         return pairs;
     }
